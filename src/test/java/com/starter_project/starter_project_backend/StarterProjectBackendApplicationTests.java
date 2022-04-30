bb922a1368bbb077f2f2bc0c65a0c36e76640024
jmunkc <jona04g8@stud.kea.dk>
2022-04-30 13:46:15 +0200

    Initial commit

---
+++ src/test/java/com/starter_project/starter_project_backend/StarterProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter_project.starter_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
